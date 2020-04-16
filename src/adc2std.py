a636d8a54869cda9a9e12afd8e6f34efe45f6dbe
michele <sload@smartload1>
2020-04-16 19:08:36 +0300

    Add mapping to adc

---
+++ src/adc2std.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Header
+from std_msgs.msg import Int32, Bool
+from std_msgs.msg import String
+from sensor_msgs.msg import Joy
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import PoseStamped, TwistStamped
+from mavros_msgs import msg
+from datetime import datetime
+import math
+from geometry_msgs.msg import Pose, Point, Quaternion, Vector3
+import src.Unity2RealWorld as toRW
+from joy2rc.msg import IntAdc
+
+class Adc2Std(object):
+    bladeImuMsg = Imu()
+    msgHeightShort = Int32()
+    msgHeightLong = Int32()
+    thismsg = IntAdc()
+
+    def AdcSubCB(self, adc):
+        self.msgHeightShort = adc[0]
+        self.pubShortHeight.publish(self.msgHeightShort)
+        self.msgHeightLong = adc[1]
+        self.pubLongHeight.publish(self.msgHeightLong)
+
+        rollPitchYaw = Vector3(adc[2], adc[3], adc[4])
+        self.bladeImuMsg.orientation = toRW.rotationROS2RW(rollPitchYaw)
+        self.pubBladeImu.publish(self.bladeImuMsg)
+
+
+    def __init__(self):
+        rospy.init_node('adc2std', anonymous=False, log_level=rospy.DEBUG)
+        # rospy.init_node('slagent', anonymous=False,log_level=rospy.DEBUG)
+
+        # Define Subscriber
+        self.adcSub = rospy.Subscriber('/adc', IntAdc, self.AdcSubCB)
+
+        # Define Publisher /mavros/rc/override (mavros/OverrideRCIn)
+        self.pubShortHeight = rospy.Publisher("/arm/height", Int32, queue_size=10)
+        self.pubLongHeight = rospy.Publisher("/arm/longHeight", Int32, queue_size=10)
+        self.pubBladeImu = rospy.Publisher("/arm/blade/Imu", Imu, queue_size=10)
+
+        # Init sequence:
+        # publish pump om 1000 then on 2000
+
+        rospy.spin()
+
+if __name__ == '__main__':
+    node = Adc2Std()
